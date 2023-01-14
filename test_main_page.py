9c188676d5007cf22e5847d0be921a944ce58644
a.mikshunas <a.mikshunas@gmail.com>
2023-01-14 15:58:47 +0300

    Add base_page, main_page; modify test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
-# pytest -v --tb=line --language=en test_main_page.py
-
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser):    
     browser.get(link)
+    go_to_login_page(browser)
+    
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
